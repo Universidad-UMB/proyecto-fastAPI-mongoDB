fb22882a6a6ea849cb4a1b9afea8a200b2928f00
Franklin <franklin2908david@gmail.com>
2025-04-08 03:10:03 -0500

    Se cambia validacion de certicados desde la URI de conexion de mongo

--- db/database.py
+++ db/database.py
@@ -1,5 +1,5 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_URI = "mongodb+srv://franklin:root@cluster0.l7g9j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_URI = "mongodb+srv://franklin:root@cluster0.l7g9j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tls=true&tlsAllowInvalidCertificates=true"
 client = AsyncIOMotorClient(MONGO_URI)
 collectionItem = client["laboratorio_ing_web"]
